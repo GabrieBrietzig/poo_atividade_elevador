2dd6a6ab8f01c3f22d8d0f9becac0c93b20210c8
GabrieBrietzig <gabriel.brietzig@univille.br>
2026-03-20 19:57:19 -0300

    Refactor Elevador class with new structure and methods

--- Elevador.java
+++ Elevador.java
@@ -1,95 +1,70 @@
-import java.util.Scanner;
-
 public class Elevador {
-    int AndarAtual;
-    int TotalAndares;
-    int AndarDesejado;
-    boolean portaAberta;
 
-    Scanner scanner = new Scanner(System.in);
+    private int andarAtual;
+    private int limiteInferior;
+    private int limiteSuperior;
+    private boolean portaAberta;
+
+    public Elevador(int limiteInferior, int limiteSuperior) {
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        this.andarAtual = limiteInferior;
+        this.portaAberta = false;
+    }
+
+    public void subir() {
+        if (portaAberta) {
+            System.out.println("Feche a porta para subir!");
+            return;
+        }
+
+        if (andarAtual == limiteSuperior) {
+            System.out.println("Já está no último andar (" + limiteSuperior + ")");
+            return;
+        }
+
+        andarAtual++;
+        System.out.println("Subiu para o andar: " + andarAtual);
+    }
+
+    public void descer() {
+        if (portaAberta) {
+            System.out.println("Feche a porta para descer!");
+            return;
+        }
+
+        if (andarAtual == limiteInferior) {
+            System.out.println("Já está no térreo (" + limiteInferior + ")");
+            return;
+        }
+
+        andarAtual--;
+        System.out.println("Desceu para o andar: " + andarAtual);
+    }
 
     public void abrirPorta() {
         if (!portaAberta) {
             portaAberta = true;
-            System.out.println("Você abriu a porta ");
+            System.out.println("Porta aberta");
         } else {
-            System.out.println("A porta já está aberta ");
+            System.out.println("A porta já está aberta");
         }
     }
 
     public void fecharPorta() {
         if (portaAberta) {
             portaAberta = false;
-            System.out.println("Você fechou a porta ");
+            System.out.println("Porta fechada");
         } else {
-            System.out.println("A porta já está fechada ");
+            System.out.println("A porta já está fechada");
         }
     }
 
-    public void informarEstadoPorta() {
-        if (portaAberta) {
-            System.out.println("A porta está aberta ");
-        } else {
-            System.out.println("A porta está fecada ");
-        }
+    public boolean portaEstaAberta() {
+        return portaAberta;
     }
 
-    public void pegarAndar() {
-        if (portaAberta) {
-            System.out.println("Feche a porta antes de se mover ");
-            return;
-        }
-
-        System.out.println("Qual andar deseja ir?");
-        AndarDesejado = scanner.nextInt();
-
-        if (AndarDesejado >= 0 && AndarDesejado <= TotalAndares) {
-            if (AndarDesejado > AndarAtual) {
-                System.out.println("Subindo...");
-            } else if (AndarDesejado < AndarAtual) {
-                System.out.println("Desccendo...");
-            } else {
-                System.out.println("Já está nesse andar!");
-            }
-            AndarAtual = AndarDesejado;
-            System.out.println("Você está no " + AndarAtual + "º andar.");
-        } else {
-            System.out.println("Esse andar não existe. Esccolha outro.{}");
-        }
+    public void exibirAndar() {
+        System.out.println("Andar atual: " + andarAtual);
     }
-
-    public void opcoes() {
-        while (true) {
-            System.out.println("\nEscolha uma ação: ");
-            System.out.println("1 - Ir para um andar ");
-            System.out.println("2 - Abrir porta ");
-            System.out.println("3 - Fechar a porta ");
-            System.out.println("4 - Ver estado da porta ");
-            System.out.println("5 - Sair");
-
-            int opcao = scanner.nextInt();
-
-            switch (opcao) {
-                case 1:
-                    pegarAndar();
-                    break;
-                case 2:
-                    abrirPorta();
-                    break;
-                case 3:
-                    fecharPorta();
-                    break;
-                case 4:
-                    informarEstadoPorta();
-                    break;
-                case 5:
-                    System.out.println("Voce saiu");
-                    return;
-                default:
-                    System.out.println("Opçção invalida");
-            }
-        }
-    }
-
-
 }
